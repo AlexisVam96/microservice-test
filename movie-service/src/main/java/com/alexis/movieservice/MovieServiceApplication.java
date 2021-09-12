acfe5b7cbeb8330aa3373508de30afca8b03203e
AlexisVam96 <alexispajuelo6@gmail.com>
2021-09-12 18:12:09 -0500

    eureka

--- movie-service/src/main/java/com/alexis/movieservice/MovieServiceApplication.java
+++ movie-service/src/main/java/com/alexis/movieservice/MovieServiceApplication.java
@@ -2,10 +2,11 @@ package com.alexis.movieservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableEurekaClient
 public class MovieServiceApplication {
 
 	public static void main(String[] args) {
--- user-service/src/main/java/com/alexis/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/alexis/userservice/UserServiceApplication.java
@@ -2,10 +2,11 @@ package com.alexis.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
