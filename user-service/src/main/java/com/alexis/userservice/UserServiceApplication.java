e48fd1f78f9218baecfc22ec69e11597fffc7f45
AlexisVam96 <alexispajuelo6@gmail.com>
2021-09-12 18:25:21 -0500

    eureka-modified

--- user-service/src/main/java/com/alexis/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/alexis/userservice/UserServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 @EnableEurekaClient
 public class UserServiceApplication {
 
